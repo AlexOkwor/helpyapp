499eaf6f3c5336fdf49898b744efe410d4572736
AlexOkwor <47591232+AlexOkwor@users.noreply.github.com>
2023-12-05 18:06:49 +0100

    first commit on bank app

--- helpy_bank_app.py
+++ helpy_bank_app.py
@@ -1,1 +1,9 @@
 print("\n\n\t BANKING APP")
+
+#OPEN ACCOUNT
+
+Name = input("Please Enter Your First Name: ")
+Name = input("Please Enter Your Last Name: ")
+Age = input("How old are you?")
+Gender = input("What is your Gender? ")
+Phone_Number = input("Please Enter Your Phone Number: ")
