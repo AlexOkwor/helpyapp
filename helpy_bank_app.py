e5aea12f10da9f5ca57d2d1a8a3800616a5b49b6
AlexOkwor <47591232+AlexOkwor@users.noreply.github.com>
2023-12-05 18:59:56 +0100

    second update on bank app

--- helpy_bank_app.py
+++ helpy_bank_app.py
@@ -3,7 +3,16 @@ print("\n\n\t BANKING APP")
 #OPEN ACCOUNT
 
 Name = input("Please Enter Your First Name: ")
-Name = input("Please Enter Your Last Name: ")
-Age = input("How old are you?")
-Gender = input("What is your Gender? ")
-Phone_Number = input("Please Enter Your Phone Number: ")
+Name = input("\n Please Enter Your Last Name: ")
+
+import sys
+Age = int(input("\n How old are you?"))
+if(Age==15):
+    print("You are not qualified to open an account")
+else:
+    print("Thank you for banking with us")
+    sys.exit()
+
+        
+Gender = input("\n What is your Gender? ")
+Phone_Number = input("\n Please Enter Your Phone Number: ")
